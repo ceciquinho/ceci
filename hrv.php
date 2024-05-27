521d75f5a2854b18da9a5e0c5c999c038b824b5a
Your Name <you@example.com>
2024-05-27 16:54:41 -0300

    first commit

---
+++ hrv.php
@@ -0,0 +1,28 @@
+<?php
+include "header.php"
+?>
+
+
+<div class="container">
+    <h1>Hrv</h1>
+
+    <ul>
+        <li>Motorização 1.2</li>
+        <li>Combustível Gasolina</li>
+        <li>Potência (cv)230</li>
+        <li>Torque (kgf.m) 35,7</li>
+        <li>Velocidade Máxima 250km/h</li>
+        <li>Tempo 0-100 (s) 6,8</li>
+        <li>Consumo cidade (km/l) 9,9</li>
+        <li>Consumo estrada (km/l) 12,5</li>
+        <li>embreagem dupla</li>
+        <li>câmbio manual sequencial com modo automático de 6 marchas</li>
+        <li>Tração dianteira</li>
+        <li> Direção elétrica</li>
+
+    </ul>
+</div>
+
+<?php
+include "footer.php"
+?>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+include "header.php";
+?>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+
+        <div class="carousel-item active" style="background-image: url('https://revistacarro.com.br/wp-content/uploads/2019/06/Jetta-3.jpg'); min-height: 400px; background-size: cover; background-position: center;">
+            <br>
+        </div>
+
+        <div class="carousel-item" style="background-image: url('https://revistacarro.com.br/wp-content/uploads/2019/06/Jetta-3.jpg'); min-height: 400px; background-size: cover; background-position: center;">
+            <br>
+        </div>
+
+        <div class="carousel-item" style="background-image: url('https://revistacarro.com.br/wp-content/uploads/2019/06/Jetta-3.jpg'); min-height: 400px; background-size: cover; background-position: center;">
+            <br>
+        </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<div class="lucas container ">
+    <div class="row">
+        <div class="col-4">
+            <div class="card shadow p-3 mb-5 bg-white rounded" style=" border-radius: 12px;">
+                <img class="card-img-top" src="https://revistacarro.com.br/wp-content/uploads/2019/06/Jetta-3.jpg" alt="Card image cap" style="border-radius: 12px;">
+                <div class="card-body">
+                    <h5 class="card-title">Jetta GLI</h5>
+                    <p class="card-text">A partir de R$ 239.390,00</p>
+                    <a href="jetta.html" class="btn btn-primary">saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card shadow p-3 mb-5 bg-white rounded" style=" border-radius: 12px;">
+                <img class="card-img-top" src="https://www.autoo.com.br/fotos/2023/6/960_720/chevrolet_tracker_2024_1_27062023_74996_960_720.jpg" alt="Card image cap" style="border-radius: 12px;">
+                <div class="card-body">
+                    <h5 class="card-title">Tracker</h5>
+                    <p class="card-text">A partir de R$ 119.890,00</p>
+                    <a href="tracker.html" class="btn btn-primary">saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card shadow p-3 mb-5 bg-white rounded" style=" border-radius: 12px;">
+                <img class="card-img-top" src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/01/03_COROLLA_2024_1FLP4760.jpg?quality=70&strip=info" alt="Card image cap" style="border-radius: 12px;">
+                <div class="card-body">
+                    <h5 class="card-title">Corolla</h5>
+                    <p class="card-text">A partir de R$ 158.390,00</p>
+                    <a href="corolla.html" class="btn btn-primary">saiba mais</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card shadow p-3 mb-5 bg-white rounded" style=" border-radius: 12px;">
+                <img class="card-img-top" src="https://tse1.mm.bing.net/th?id=OIP.2UDG4hrmrtuBP8nKadiClgHaEK&pid=Api&P=0&h=180" alt="Card image cap" style="border-radius: 12px;">
+                <div class="card-body">
+                    <h5 class="card-title">Hrv</h5>
+                    <p class="card-text">A partir de R$ 148.900,00</p>
+                    <a href="hrv.php" class="btn btn-primary">saiba mais</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include "footer.php";
+?>
